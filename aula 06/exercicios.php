3a8d271e7a6ff1d1f17adb4c99bf52b27b346d9e
Adilson-Kina <adilsonkinakina@gmail.com>
2024-10-29 14:32:12 -0300

    change variable name for better understanding

--- aula 06/exercicios.php
+++ aula 06/exercicios.php
@@ -1,11 +1,11 @@
 <?php
   $array = array(2, 5, 7, 8, 10, 18, 30, 37, 38, 60);
-  foreach ($array as $index) {
-    if ($index % 2 == 0) {
-      echo "$index é par <br/>";
+  foreach ($array as $elemento) {
+    if ($elemento % 2 == 0) {
+      echo "$elemento é par <br/>";
     }
-    elseif ($index % 2 != 0) {
-      echo "$index é impar <br/>";
+    elseif ($elemento % 2 != 0) {
+      echo "$elemento é impar <br/>";
     }
   }
 ?>
